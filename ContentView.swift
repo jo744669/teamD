b0cff30810b9b5d6c544569dc069cf8a8a700c12
LadiBamgbose <122841175+LadiBamgbose@users.noreply.github.com>
2023-11-02 17:11:12 -0400

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SJU Ordering
+//
+//  Created by Ladi Bamgbose on 11/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SJU_OrderingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SJU_OrderingApp.swift
+//  SJU Ordering
+//
+//  Created by Ladi Bamgbose on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SJU_OrderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
