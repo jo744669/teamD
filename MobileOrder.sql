da423cca749dfa5d4b1f169a2c0c066ffc377fad
jo744669 <jo744669@sju.edu>
2023-11-26 16:34:37 -0500

    Finish populating location specific tables

--- MobileOrder.sql
+++ MobileOrder.sql
@@ -126,7 +126,7 @@ Create Table jamba_juice (
 /*
 burgers_and_fries : holds all of the data for the burgers and fries location
 */
-Create Table jamba_juice (
+Create Table burgers_and_fries (
     foreign key(resturand_id) references resturants(resturand_id),
     food_id INT PRIMARY KEY,
     food_name VARCHAR(100),
@@ -484,5 +484,134 @@ VALUES
     (7, 'Orange C Booster', 8.99, true, false, 'Orange Juice, Orange Sherbert, Peaches, Bananas, Daily Vitamin & Zinc Boost', false),
     (8, 'Greens N Ginger', 8.69, true, false, 'Lemonade, Kale, Peaches, Mango, Ginger', false),
     (9, 'PB Chocolate Love', 7.89, true, false, 'Milk, Bananas, Chocolate, Peanut Butter, Moo Dairy Base', false),
-    (10, 'Strawberry Surf Rider', 7.89, true, false, 'Lemonade, Lime, Sherbert, Strawberries, Peaches', false);
-    /*NEEDS TO BE FINISHED*/
+    (10, 'Strawberry Surf Rider', 7.89, true, false, 'Lemonade, Lime, Sherbert, Strawberries, Peaches', false),
+    (11, 'Matcha Green Tea Blast', 8.99, true, false, '"Soymilk, Fat-Free Vanilla Frozen Yogurt, Green Tea Matcha', false),
+    (12, 'Mega Mango', 8.69, true, false, 'Pineapple Juice, Orange Juice, Mango, Strawberries', false),
+    (13, 'Apple N Greens', 8.69, true, false, 'Apple, Pear, Strawberry Juice Blend, Kale, Mango, Bananas, Peaches', false),
+    (14, 'PB Banana Protein Soy', 8.99, true, false, 'Soymilk, Bananas, Peanut Butter, Whey Protein, Honey', false),
+    (15, 'Acai Super Antioxidant', 8.99, true, false, 'Soymilk, Acai, Grape Juice Blend, Strawberries, Blueberries, Raspberry Sherbert, Daily Vitamin & Zinc Boost', false),
+    (16, 'Mango-A-Go-Go', 7.89, true, false, 'Pear, White, Grape, Mango, Orange, Passion Fruit Juice Blend, Mango Sherbert, Pineapple Sherbert', false),
+    (17, 'Pomegranate Paradise', 8.69, true, false, 'Apple, Pear Juice Blend, Pomegranate Juice blend, Mango, Strawberries, Peaches', false),
+    (18, 'Strawberries Wild', 7.89, true, false, 'Apple, Pear Juice Blend, Strawberry Juice Blend, Fat-Free Vanilla Frozen Yogurt, Strawberries, Bananas', false),
+    (19, 'Peach Perfection', 8.69, true, false, 'White Grape, Pear Juice Blend, Peach Juice Blend, Mango, Peaches, Apple-Strawberry Juice Blend, Strawberries', false),
+    (20, 'White Gummi', 7.89, true, false, 'White Grape, Peach Juice Blend, Pear Fruit Juice Blend, Pineapple Sherbert, Vanilla, Soymilk, Orange Sherbert, Raspberry Sherbert, Mangos and Lime Sherbert', false),
+    (21, 'Vanilla Blue Sky', 8.69, true, false, 'Unsweetened Almond Milk, Vanilla, Coconut Milk, Bananas, Pineapple, Blue Spirulina Boost', false),
+    (22, 'Strawberry Whirl', 8.69, true, false, 'Apple, Pear Juice Blend, Strawberry Juice Blend, Strawberries, Bananas', false),
+    (23, 'Smooth Talkin Mango', 8.69, true, false, 'Oatmilk, Mango, Pineapple, Organic Agave', false),
+    (24, 'Peanut Butter Moo', 7.89, true, false, 'Fat-Free Vanilla Frozen Yogurt, Vanilla, Soymilk, Milk, Bananas, Peanut Butter, Chocolate, Moo Dairy Base', false),
+    (25, 'Protein Berry Workout Pea', 8.99, true, false, 'Vanilla, Soymilk, Pea Protein, Strawberries, Bananas', false),
+    (26, 'Protein Berry Workout Whey', 8.99, true, false, 'Vanilla, Soymilk, Whey Protein, Strawberries, Bananas', false),
+    (27, 'Chucky Strawberry Bowl', 8.99, false, true, 'No description available', false),
+    (28, 'Acai Primo Bowl', 8.99, false, true, 'No description available', false),
+    (29, 'Vanilla Blue Sky Bowl', 8.99, false, true, 'No description available', false),
+    (30, 'Dragonfruit Delight Bowl', 8.99, false, true, 'No description available', false),
+    (31, 'Three G Energy', 0, false, false, 'No description available', true),
+    (32, 'Daily Vitamin Zinc', 0, false, false, 'No description available', true),
+    (33, 'Soy Protein', 0, false, false, 'No description available', true),
+    (34, 'Whey Protein', 0, false, false, 'No description available', true),
+    (35, 'Blue Spirulina', 0.75, false, false, 'No description available', true),
+    (36, 'Ginger', 0.75, false, false, 'No description available', true),
+    (37, 'Kale', 0.75, false, false, 'No description available', true),
+    (38, 'Matcha Powder', 0.75, false, false, 'No description available', true),
+    (39, 'NonFat Greek Yogurt', 0.75, false, false, 'No description available', true),
+    (40, 'Peanut Butter', false, false, 'No description available', true);
+
+/*
+populating the burgers and fries table
+*/
+INSERT INTO burgers_and_fries (food_id, food_name, price, main_dish_single, main_dish_combo, side_dish, drink)
+VALUES
+    (1, "Nash Spicy Chicken", 5.29, true, false, false, false),
+    (2, "Grilled Chicken and Bacon", 7.09, true, false, false, false),
+    (3, "Crispy Fish Sandwich", 6.99, true, false, false, false),
+    (4, "Kosher Beef Hot Dog", 4.29, true, false, false, false),
+    (5, "Burger with Cheese", 5.99, true, false, false, false),
+    (6, "Burger with Bacon and Cheese", 6.49, true, false, false, false),
+    (7, "Burger with Avocado and Pepper Jack", 6.49, true, false, false, false),
+    (8, "Double Burger with Cheese", 6.49, true, false, false, false),
+    (9, "Chipotle Black Bean and Cheese", 7.09, true, false, false, false),
+    (10, "CYO Sandwich", 5.89, true, false, false, false),
+    (11, "Grilled Cheese", 5.99, true, false, false, false),
+    (12, "Beyond and Cheese Burger", 9.49, true, false, false, false),
+    (13, "Patty Melt", 6.49, true, false, false, false),
+    (14, "Philly Cheesesteak", 7.29, true, false, false, false),
+    (15, "Chicken Tender Sandwich", 5.29, true, false, false, false),
+    (16, "Crispy Chicken Strips (3)", 4.59, true, false, false, false),
+    (17, "Crispy Chicken Strips (5)", 7.29, true, false, false, false),
+    (18, "Grilled Chicken and Bacon", 10.19, false, true, false, false),
+    (19, "Tender Sandwich", 8.29, false, true, false, false),
+    (20, "Chipotle Black Bean and Cheese", 10.59, false, true, false, false),
+    (21, "Burger with Bacon and Cheese", 10.09, false, true, false, false),
+    (22, "Burger with Avocado and Pepper Jack", 10.09, false, true, false, false),
+    (23, "Burger with Cheese", 9.29, false, true, false, false),
+    (24, "Chicken Cheesesteak", 7.29, false, true, false, false),
+    (25, "Double Burger with Cheese", 12.09, false, true, false, false),
+    (26, "CYO Sandwich", 8.89, false, true, false, false),
+    (27, "Beyond and Cheese Burger", 12.79, false, true, false, false),
+    (28, "Crispy Chicken Strips (3)", 4.59, false, true, false, false),
+    (29, "Crispy Chicken Strips (5)", 7.29, false, true, false, false),
+    (30, "Motzarella Sticks", 6.09, false, false, true, false),
+    (31, "Classic Fries", 3.19, false, false, true, false),
+    (32, "Sweet Potato Fries", 3.49, false, false, true, false),
+    (33, "Ranch Fries", 2.69, false, false, true, false),
+    (34, "Medium Fountain Drink", 2.99, false, false, false, true),
+    (35, "Large Fountain Drink", 3.29, false, false, false, true);
+
+/*
+populating the spread table
+*/
+
+INSERT INTO the_spread (food_id, food_name, price, main_dish, side_dish, drink)
+VALUES
+    (1, "6in Four Cheese", 5.79, true, false, false),
+    (2, "6in Ham + American", 7.49, true, false, false),
+    (3, "6in Trukey + Swiss", 7.49, true, false, false),
+    (4, "6in Turkey + Baco on Multigrain", 8.59, true, false, false),
+    (5, "6in Italian", 7.99, true, false, false),
+    (6, "12in Artichoke Red Pepper Sub", 16.09, true, false, false),
+    (7, "6in Caprese Ciabatta", 8.59, true, false, false),
+    (8, "12in Caprese Ciabatta", 16.09, true, false, false),
+    (9, "6in Roast Beef + Cheddar", 7.99, true, false, false),
+    (10, "12in Buffalo Chicken Sub", 16.09, true, false, false),
+    (11, "12in Ham + American", 14.49, true, false, false),
+    (12, "6in Tuna Salad", 7.49, true, false, false),
+    (13, "12in Chicken Caesar Wrap", 16.09, true, false, false),
+    (14, "12in Four Cheese", 10.79, true, false, false),
+    (15, "12in Signature Italian Sub", 16.79, true, false, false),
+    (16, "12in Italian", 14.99, true, false, false),
+    (17, "12in Turkey + Bacon on Multigrain", 16.09, true, false, false),
+    (18, "6in Buffalo Chicken Sub", 8.59, true, false, false),
+    (19, "12in Roast Beef + Cheddar", 14.99, true, false, false),
+    (20, "12in Tuna Salad", 14.49, true, false, false),
+    (21, "12in Turkey + Swiss", 14.49, true, false, false),
+    (22, "6in Chicken Caesar Wrap", 8.59, true, false, false),
+    (23, "6in Artichoke + Red Pepper Sub", 8.59, true, false, false),
+    (24, "6in Signature Italian Sub", 9.29, true, false, false),
+    (25, "Signature Chips", 0, false, true, false),
+    (26, "Crave Worthy Chocolate Chip Cookie", 0, false, true, false),
+    (27, "Medium Fountin Drink", 2.99, false, false, true),
+    (28, "Large Fountain Drink", 3.29, false, false, true);
+
+/*
+populating the auntie annes table
+*/
+INSERT INTO auntie_annes (food_id, food_name, price, main_dish, sauce, drink)
+VALUES
+    (1, "Pretzel", 4.29, true, false, false),
+    (2, "Original Pretzel Dogs", 4.99, true, false, false),
+    (3, "Mini Dogs", 6.59, true, false, false),
+    (4, "Nuggest Cup - Original", 5.79, true, false, false),
+    (5, "Nuggets Cup - Cinnamon Sugar", 5.89, true, false, false),
+    (6, "Nuggets Cup - Pepperoni", 6.59, true, false, false),
+    (7, "Nuggets Bucket - Original", 26.99, true, false, false),
+    (8, "Nuggets Bucket - Cinnamon Sugar", 27.99, true, false, false),
+    (9, "Nuggets Bucket - Pepperoni", 32.99, true, false, false),
+    (10, "Caramel", 1.19, false, true, false),
+    (11, "Cheese", 1.19, false, true, false),
+    (12, "Honey Mustard", 1.19, false, true, false),
+    (13, "Hot Salsa Cheese", 1.19, false, true, false),
+    (14, "Marinara", 1.19, false, true, false),
+    (15, "Medium Fountain Drink", 2.99, false, false, true),
+    (16, "Large Fountain Drink", 3.29, false, false, true),
+    (17, "Original Lemonade", 3.99, false, false, true),
+    (18, "Lemonade Mixer", 4.39, false, false, true);
