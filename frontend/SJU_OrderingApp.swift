4a8ee1276e526c41b74cd447a183dec3669d0f8e
LadiBamgbose <122841175+LadiBamgbose@users.noreply.github.com>
2023-11-29 22:05:13 -0500

    Update SJU_OrderingApp.swift

--- frontend/SJU_OrderingApp.swift
+++ frontend/SJU_OrderingApp.swift
@@ -1,15 +1,11 @@
 import SwiftUI
 
+
 @main
 struct SJU_Orderingapp: App {
-    // Create an instance of Cart
-    @StateObject private var cart = Cart()
-
     var body: some Scene {
         WindowGroup {
-            // Set Cart as an environment object
-            ContentView()
-                .environmentObject(cart)
+            LoginView()
         }
     }
 }
