0131065a4b38a0e0496e4c6c7a5a5d0d8ad8d612
LadiBamgbose <122841175+LadiBamgbose@users.noreply.github.com>
2023-12-11 21:18:56 -0500

    Update LoginView.swift

--- frontend/LoginView.swift
+++ frontend/LoginView.swift
@@ -6,6 +6,9 @@ struct LoginView: View {
     @State private var password: String = ""
     @State private var loginError: Bool = false
     @State private var isMainTabViewPresented: Bool = false
+    @State private var isRegistrationViewPresented: Bool = false
+    @Binding var userLoggedInEmail: String?
+    
 
     var body: some View {
         VStack {
@@ -26,17 +29,20 @@ struct LoginView: View {
                 .background(Color.gray)
 
             Button("Sign In") {
-                // Check email and password (replace with actual authentication logic)
-                if isValidLogin() {
-                    withAnimation {
-                        print("Valid login. Logging in...")
-                        isMainTabViewPresented = true
-                        loginError = false
-                    }
-                } else {
-                    withAnimation {
-                        print("Invalid login. Check email and password.")
-                        loginError = true
+                // Check email and password using isValidLogin
+                isValidLogin { success in
+                    if success {
+                        withAnimation {
+                            print("Valid login. Logging in...")
+                            userLoggedInEmail = email
+                            isMainTabViewPresented = true
+                            loginError = false
+                        }
+                    } else {
+                        withAnimation {
+                            print("Invalid login. Check email and password.")
+                            loginError = true
+                        }
                     }
                 }
             }
@@ -49,24 +55,176 @@ struct LoginView: View {
                     .stroke(loginError ? Color.red : Color.clear, lineWidth: 2)
             )
             .padding(.top, 20)
+
+
+            // Add registration button
+            Button("Register") {
+                isRegistrationViewPresented.toggle()
+            }
+            .padding()
+            .foregroundColor(.blue)
         }
         .padding()
         .fullScreenCover(isPresented: $isMainTabViewPresented, content: {
             MainTabView(userEmail: email)
         })
+        .fullScreenCover(isPresented: $isRegistrationViewPresented, content: {
+            RegistrationView(isPresented: $isRegistrationViewPresented)
+        })
+    }
+    private func isValidLogin(completion: @escaping (Bool) -> Void) {
+        guard let url = URL(string: "http://localhost:3000/signin") else {
+            print("Invalid URL")
+            completion(false)
+            return
+        }
+
+        let userData = ["email": email, "password": password]
+        let jsonData = try? JSONSerialization.data(withJSONObject: userData)
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = jsonData
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error: \(error)")
+                completion(false)
+                return
+            }
+
+            if let data = data {
+                let responseString = String(data: data, encoding: .utf8)
+                print("Response: \(responseString ?? "")")
+
+                // Handle the response here and return true or false based on authentication result
+                // You might need to parse the response JSON and check if the authentication was successful
+
+                // Example: Assuming your response is a JSON with a "success" key
+                if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                   let success = json["success"] as? Bool {
+                    completion(success)
+                    return
+                }
+            }
+
+            completion(false)
+        }
+        .resume()
     }
 
-    private func isValidLogin() -> Bool {
-        // Add your logic to check email and password
-        // For example, return true if the email has "@sju.edu" suffix and password is "password"
-        return email.lowercased().hasSuffix("@sju.edu") && password == "password"
+    
+
+    
+    private func signInUser(email: String, password: String, completion: @escaping (Result<Data, Error>) -> Void) {
+            guard let url = URL(string: "http://localhost:3000/signin") else {
+                print("Invalid URL")
+                return
+            }
+
+            let userData = ["email": email, "password": password]
+            let jsonData = try? JSONSerialization.data(withJSONObject: userData)
+
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.httpBody = jsonData
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+
+                if let data = data {
+                    completion(.success(data))
+                }
+            }
+            .resume()
+        }
+    
+}
+
+struct RegistrationView: View {
+    @Binding var isPresented: Bool
+    @State private var newUsername: String = ""
+    @State private var newPassword: String = ""
+
+    var body: some View {
+        VStack {
+            Text("Register")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+
+            TextField("Username", text: $newUsername)
+                .padding()
+                .font(.title3)
+                .background(Color.gray)
+                .autocapitalization(.none)
+                .disableAutocorrection(true)
+
+            SecureField("Password", text: $newPassword)
+                .padding()
+                .font(.title3)
+                .background(Color.gray)
+
+            Button("Register") {
+                // Perform registration logic
+                registerUser(email: newUsername, password: newPassword) { result in
+                    switch result {
+                    case .success(let data):
+                        // Handle success if needed
+                        print("Registration success: \(data)")
+
+                        // Dismiss the registration view and navigate back to login
+                        isPresented = false
+                    case .failure(let error):
+                        // Handle failure if needed
+                        print("Registration error: \(error.localizedDescription)")
+                    }
+                }
+            }
+
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+        }
+        .padding()
     }
+    private func registerUser(email: String, password: String, completion: @escaping (Result<Data, Error>) -> Void) {
+            guard let url = URL(string: "http://localhost:3000/newUser") else {
+                print("Invalid URL")
+                return
+            }
+
+            let userData = ["email": email, "password": password]
+            let jsonData = try? JSONSerialization.data(withJSONObject: userData)
+
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.httpBody = jsonData
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+
+                if let data = data {
+                    completion(.success(data))
+                }
+            }
+            .resume()
+        }
 }
 
 #if DEBUG
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        LoginView(userLoggedInEmail: .constant(""))
     }
 }
 #endif
