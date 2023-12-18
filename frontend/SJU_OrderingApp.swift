ebb4b3ecb4c3fcd92f0646777167cd2da68903f1
LadiBamgbose <122841175+LadiBamgbose@users.noreply.github.com>
2023-12-18 02:18:58 -0500

    Update SJU_OrderingApp.swift

--- frontend/SJU_OrderingApp.swift
+++ frontend/SJU_OrderingApp.swift
@@ -1,11 +1,13 @@
 import SwiftUI
 
-
 @main
 struct SJU_Orderingapp: App {
+    @StateObject private var userEmailManager = UserEmailManager.shared
+    
     var body: some Scene {
         WindowGroup {
-            LoginView(userLoggedInEmail: .constant(""))
+            LoginView()
+                .environmentObject(userEmailManager)
         }
     }
 }
